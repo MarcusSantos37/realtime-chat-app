bd35191e8de6b2105e60c9b63872720cc8375f8e
marcus.augusto <marcus.augusto@globalsys.com.br>
2023-03-27 21:38:55 -0300

    join chat and send message - backend

--- server/index.js
+++ server/index.js
@@ -6,4 +6,26 @@ const io = require("socket.io")(server, {
 
 const PORT = 3001;
 
+io.on("connection", (socket) => {
+  console.log("Usuário conectado!", socket.id);
+
+  socket.on("disconnect", (reason) => {
+    console.log("Usuário desconectado!", socket.id);
+  });
+
+  socket.on("setUsername", (username) => {
+    socket.data.username = username;
+
+    console.log(socket.data.username);
+  });
+
+  socket.on("message", (text) => {
+    io.emit("receivedMessage", {
+      text,
+      authorId: socket.id,
+      author: socket.data.username,
+    });
+  });
+});
+
 server.listen(PORT, () => console.log("Server running..."));
